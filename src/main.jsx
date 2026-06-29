4acc8c87af3e3e33075ee3b602707512abc15688
Auto Lavage Casa <dev@autolavage-casa.ma>
2026-06-29 20:11:26 +0000

    debug: minimal React app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+function App() {
+  return React.createElement('div', { style: { color: 'white', padding: 40, fontSize: 32 } }, 'HELLO FROM REACT')
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(React.createElement(App))
